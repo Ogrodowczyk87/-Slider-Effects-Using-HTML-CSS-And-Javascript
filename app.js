52b586717f44228c3a2fa54742d2e5e49486ad71
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2024-03-03 19:32:12 +0000

    animation bug

---
+++ app.js
@@ -0,0 +1,36 @@
+let nexDom = document.getElementById('next');
+let prevDom = document.getElementById('prev')
+let carouselDom = document.querySelector('carousel')
+let listItemDom = document.querySelector('.carousel .list')
+let thumbnailDom = document.querySelector('.carousel .thumbnail')
+
+nexDom.onclick = function () {
+    showSlider('next')
+}
+
+prevDom.onclick = function () {
+    showSlider('prev')
+}
+let timeRunnig = 3000;
+let runTimeOut;
+
+function showSlider(type) {
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item')
+
+    if (type === 'next') {
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carouselDom.classList.add('next')
+    } else {
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSlider[positionLastItem])
+        .carouselDom.classList.add('prev')
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next')
+    }, timeRunnig)
+
+}
